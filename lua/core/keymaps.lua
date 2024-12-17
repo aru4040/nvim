e16ede72bd6763ff47bc4cd3d42367db207effad
arubek <4040gold.dev@gmail.com>
2024-12-18 02:23:38 +0900

    fix keymap(buffer split)

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,8 +5,8 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "上のwindowに切り替え" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "下のwindowに切り替え" })
 
 -- windowの追加
-vim.keymap.set("n", "<leader>v", ":Ve<CR>", { desc = "windowを左右に分割" })
-vim.keymap.set("n", "<leader>s", ":Se<CR>", { desc = "windowを上下に分割" })
+vim.keymap.set("n", "<leader>v", ":vsp<CR>", { desc = "windowを左右に分割" })
+vim.keymap.set("n", "<leader>s", ":sp<CR>", { desc = "windowを上下に分割" })
 
 -- バッファの移動
 vim.keymap.set("n", "[b", ":bprev<CR>", { desc = "前のバッファへ移動" })
